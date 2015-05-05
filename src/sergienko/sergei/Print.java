ffb0b491386fb01108ecc77b23b695c5c8cef806
SerhII <sergienkosb@gmail.com>
2015-05-05 14:33:51 +0300

    add Print

---
+++ src/sergienko/sergei/Print.java
@@ -0,0 +1,16 @@
+package sergienko.sergei;
+
+/**
+ * Created by user on 05.05.2015.
+ */
+public class Print {
+    public static void print(Object obj){
+        System.out.println(obj);
+    }
+    public static void print(){
+        System.out.println();
+    }
+    public static void printnb(Object obj){
+        System.out.print(obj);
+    }
+}
