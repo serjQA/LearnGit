781ffa1060556272f25d75cfe358e2cf1a67c082
SerhII <sergienkosb@gmail.com>
2015-05-08 12:33:37 +0300

    add MainClass

--- src/Main.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by user on 06.05.2015.
- */
-public class Main {
-    public static void main(String [] args){
-        
-    }
-}
---
+++ src/MainClass.java
@@ -0,0 +1,8 @@
+/**
+ * Created by user on 06.05.2015.
+ */
+public class MainClass {
+    public static void main(String[] args) {
+       
+    }
+}
